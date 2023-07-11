a9290de4e49c30c5a8b513476ed1c2ce99589803
Alex2 <aleksander1802@gmail.com>
2023-07-12 01:57:56 +0300

    feat: added correct display of surrounding miss cells

--- src/websocket/updateSurroundingCells.ts
+++ src/websocket/updateSurroundingCells.ts
@@ -1,4 +1,4 @@
-import { GameMatrix } from './checkAttack.ts';
+import { GameMatrix } from '../../src/model/types/matrix.ts';
 
 export function updateSurroundingCells(
   matrix: GameMatrix,
@@ -8,46 +8,35 @@ export function updateSurroundingCells(
   const numRows = matrix.length;
   const numCols = matrix[0].length;
 
-  // Обновляем статус ячейки слева
   if (x > 0 && matrix[y][x - 1] !== 'killed') {
     matrix[y][x - 1] = 'miss';
   }
 
-  // Обновляем статус ячейки справа
   if (x < numCols - 1 && matrix[y][x + 1] !== 'killed') {
     matrix[y][x + 1] = 'miss';
   }
 
-  // Обновляем статус ячейки сверху
   if (y > 0 && matrix[y - 1][x] !== 'killed') {
     matrix[y - 1][x] = 'miss';
   }
 
-  // Обновляем статус ячейки снизу
   if (y < numRows - 1 && matrix[y + 1][x] !== 'killed') {
     matrix[y + 1][x] = 'miss';
   }
 
-  // Обновляем статус ячейки в левом верхнем углу
   if (x > 0 && y > 0 && matrix[y - 1][x - 1] !== 'killed') {
     matrix[y - 1][x - 1] = 'miss';
   }
 
-  // Обновляем статус ячейки в правом верхнем углу
   if (x < numCols - 1 && y > 0 && matrix[y - 1][x + 1] !== 'killed') {
     matrix[y - 1][x + 1] = 'miss';
   }
 
-  // Обновляем статус ячейки в левом нижнем углу
   if (x > 0 && y < numRows - 1 && matrix[y + 1][x - 1] !== 'killed') {
     matrix[y + 1][x - 1] = 'miss';
   }
 
-  // Обновляем статус ячейки в правом нижнем углу
   if (x < numCols - 1 && y < numRows - 1 && matrix[y + 1][x + 1] !== 'killed') {
     matrix[y + 1][x + 1] = 'miss';
   }
 }
-
-
-  
