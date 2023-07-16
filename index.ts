d05ba99bf012bdc328a44468f4965346fc9d7324
Alex2 <aleksander1802@gmail.com>
2023-07-16 17:46:07 +0300

    refactor: removed auto browser opening

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { httpServer } from './src/http_server/index.ts';
 import dotenv from 'dotenv';
 import './src/websocket/server.ts';
-import { exec } from 'child_process';
 
 dotenv.config();
 
@@ -9,17 +8,4 @@ export const HTTP_PORT = parseInt(process.env.PORT || '8181');
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 
-httpServer.listen(HTTP_PORT, () => {
-  const openCommand = process.platform === 'win32' ? 'start' : 'xdg-open';
-
-  exec(`${openCommand} http://localhost:${HTTP_PORT}`, (error) => {
-    if (error) {
-      console.error(`Failed to open browser: ${error}`);
-    }
-  });
-  exec(`${openCommand} http://localhost:${HTTP_PORT}`, (error) => {
-    if (error) {
-      console.error(`Failed to open browser: ${error}`);
-    }
-  });
-});
+httpServer.listen(HTTP_PORT);
